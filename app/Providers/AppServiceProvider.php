25c3e9025171b91d11eaddbf20d34a4291058c8b
koudjo <kametepe@gmail.com>
2019-12-21 00:41:06 +0100

    use ElasticSearch for Article Search

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 use App\Repository;
 
+use Elasticsearch\Client;
+use Elasticsearch\ClientBuilder;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,10 +16,29 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(
-            Repository\IArticlesRepository::class,
-            Repository\EloquentRepository::class
-        );
+        $this->app->bind(Repository\IArticlesRepository::class, function () {
+            // This is useful in case we want to turn-off our
+            // search cluster or when deploying the search
+            // to a live, running application at first.
+            if (! config('services.search.enabled')) {
+                return new Repository\EloquentRepository();
+            }
+
+            return new Repository\ElasticsearchRepository(
+                $this->app->make(Client::class)
+            );
+        });
+
+        $this->bindSearchClient();
+    }
+
+    private function bindSearchClient()
+    {
+        $this->app->bind(Client::class, function ($app) {
+            return ClientBuilder::create()
+                ->setHosts($app['config']->get('services.search.hosts'))
+                ->build();
+        });
     }
 
     /**
